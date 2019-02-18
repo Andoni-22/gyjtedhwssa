55c2c330294ad112528d391cea2c9a83495378a8
Andoni-22 <47524068+Andoni-22@users.noreply.github.com>
2019-02-18 12:06:09 +0100

    Add files via upload

---
+++ Nombre.java
@@ -0,0 +1,18 @@
+package empresaxxxxxx;
+
+public class Nombre {
+	private String nombre;
+	private int veces;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getVeces() {
+		return veces;
+	}
+	public void setVeces(int veces) {
+		this.veces = veces;
+	}
+}
